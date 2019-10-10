14ad40c5e73396df256568d0a24a54a1db934bc8
Alex Enrique <alex@debian.alex>
2019-10-10 12:20:59 -0300

    Added Map class description

--- map.d
+++ map.d
@@ -1,4 +1,26 @@
 module map;
+
+/**
+    Range template class Map. It aims to store all the map information
+    and update methods of a one dimensional map
+
+    Temple values:
+      alis f -- f must be a __lambda1 callable (one argument)
+
+    Members:
+      private real _x : the current value of the map
+      (the current value of the orbit)
+
+      enum bool empty : range parameter for emptiness
+      (it is always false as Map is a infinity range)
+
+      @property real front : get the current value of _x
+
+      void popFront() : update the value of _x
+
+      @property bool isAtFixedPoint : returns true if the
+      current value of _x is a fixed point 
+*/
 class Map(alias f) {
     private real _x;
     enum bool empty = false;

91ba02a6cd13c2e94bacde33933ec948677cd078
Alex Enrique <alex@debian.alex>
2019-10-10 12:33:04 -0300

    Added unit tests

--- map.d
+++ map.d
@@ -19,7 +19,7 @@ module map;
       void popFront() : update the value of _x
 
       @property bool isAtFixedPoint : returns true if the
-      current value of _x is a fixed point 
+      current value of _x is a fixed point
 */
 class Map(alias f) {
     private real _x;
@@ -40,4 +40,16 @@ class Map(alias f) {
     @property bool isAtFixedPoint() const {
         return _x == f(_x);
     }
+}
+
+unittest {
+    import std.range : take;
+    import std.algorithm.comparison : equal;
+    import std.math : approxEqual;
+    auto map = new Map!(x => 2*x*(1-x))(0.01);
+    assert(map.take(12).approxEqual([0.01000, 0.01980, 0.0388159, 0.0746185,
+                                     0.138101, 0.238058, 0.362773, 0.462338,
+                                     0.497163, 0.499984, 0.50000, 0.50000]));
+    //
+    assert(map.isAtFixedPoint);
 }

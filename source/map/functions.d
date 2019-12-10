41be4b5a91cc990cd629a1cdc0889cc6f7e77e33
Alex Enrique <alex@debian.alex>
2019-12-10 12:09:36 -0300

    Changed dependencies

--- source/map/functions.d
+++ source/map/functions.d
@@ -1,12 +1,17 @@
 module map.functions;
 import std.math;
 import std.traits : isIntegral;
-import scid.calculus : diff;
+
+/* BUG: Crashed. See Below
+import scid.calculus : diff; */
 
 import map.map;
-import map.logistic;
-import map.primitives;
+import map.logistic_map;
+import map.traits;
 
+
+/*
+BUG: It was crashed by the new implementation of Map(alias f) for generic types
 
 bool isSink(alias f)(Map!f map, real point) {
     auto func = function real(real x) { return f(x); };
@@ -17,6 +22,18 @@ bool isSource(alias f)(Map!f map, real point) {
     auto func = function real(real x) { return f(x); };
     return abs(func.diff(point).value) > 1;
 }
+*/
+
+
+/* BUG: Crashed. See above.
+unittest {
+    auto map = logisticMap(2);
+    map.setInitialPoint(.01);
+
+    assert(map.isSink(.5));
+    assert(map.isSource(0));
+}
+*/
 
 
 // Convenience
@@ -27,9 +44,8 @@ alias time = times;
 }
 
 unittest {
-    auto map = new LogisticMap!(2);
-    assert(map.isSink(.5));
-    assert(map.isSource(0));
-
     assert(5.times == 5);
+    assert(1564.times == 1564);
+    assert(1.times == 1);
+    assert(55.time == 55);
 }

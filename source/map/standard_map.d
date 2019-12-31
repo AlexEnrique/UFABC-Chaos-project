00d138e886ce94375937876cf203369ae178c380
Alex Enrique <41076109+AlexEnrique@users.noreply.github.com>
2019-12-31 16:37:05 -0300

    Implement the standard map fixing and sensitive dependence tasks (#5)
    * Added time() and times() functions for convenience in writing
    
    * Added temporary folder to develop parallel classes/structs/functions... Added a N-dimensional variant for the Map(alias f) class (to become the principal class)
    
    * Class adaptader to 1-dimensional maps. There are still some bugs to fix and thing to implement (e.g. slipOp)
    
    * Changed dependencies
    
    * 'Merge' module map.temp.mapNDim to map.map
    
    * 'Merge' module map.temp.mapNDim to map.map
    
    * Changed dependencies
    
    * Changed map.primitives to map.traits
    
    * Logistic map and Standard map implementations
    
    * void main()
    
    * source/map/temp and backup-files/ added to .gitignore
    
    * New figures for the pendulum, the standard map, etc.
    
    * Main changed to request tasks
    
    * changes for implementation of the standard map
    
    * ODE Runge-Kutta solver
    
    * Division by tasks (called from main)
    
    * figures/standard_map.xcf added to .gitignore
    
    * New figures and standard map phase portrait
    
    * Pendulum phase portrait modified
    
    * Standard map fixed
    
    * Fixed typos in map.map. Added new tasks to main.d
    
    * Tent map implementation
    
    * Sensitive dependence study tasks added
    
    * rm figures/standard_map.xcf (large file)
    
    * Update README.md
    
    * Remove temp files

--- source/map/standard_map.d
+++ source/map/standard_map.d
@@ -10,12 +10,11 @@ private alias ThetaPTuple = Tuple!(real, "theta", real, "p");
 
 auto standardMap(real k) {
     return new Map!( (real theta, real p) =>
-        ThetaPTuple((theta + p) % 2 * PI, p + k * sin(theta + p)) );
+        ThetaPTuple((theta + p) % (2 * PI), (p + k * sin(theta + p)) ) );
 }
 
 unittest {
-    real k = 0.1;
-    auto stdMap = standardMap(k);
+    auto stdMap = standardMap(0.1);
     stdMap.setInitialPoint(0, 0);
     assert(stdMap.isAtFixedPoint);
 
